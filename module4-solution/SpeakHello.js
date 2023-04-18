a8b68a5831678a4ebc653199d9d4e9e14343e65b
bobbychaichai <126515753+bobbychaichai@users.noreply.github.com>
2023-04-18 14:14:40 -0400

    Update SpeakHello.js

--- module4-solution/SpeakHello.js
+++ module4-solution/SpeakHello.js
@@ -26,5 +26,7 @@ helloSpeaker.speak function (name) {
 // See Lecture 52, part 2
 // (Note, Step 6 will be done in the SpeakGoodBye.js file.)
 // xxxx.xxxx = helloSpeaker;
+  
+window.helloSpeaker = helloSpeaker;
 
 })();
